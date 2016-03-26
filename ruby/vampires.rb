dc843ce6b64b8e18528c071c04fe7d56eab8ccb6
Walter Chabla <walterchabla43@gmail.com>
2016-03-25 20:37:03 -0400

    program created to find out information about hamster

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+old = gets.chomp
+
+puts "What year were you born?"
+born = gets.chomp.
+
+puts "Our company serves garlic. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurence?"
+insurence = gets.chomp
