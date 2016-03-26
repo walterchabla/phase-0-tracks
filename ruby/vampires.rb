a1426c5cefed9c114098b5761cfbbf1b68c7f022
Walter Chabla <walterchabla43@gmail.com>
2016-03-25 20:38:23 -0400

    still working on this file

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,29 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-old = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-born = gets.chomp.
+year_born = gets.chomp.to_i
+
+#first exercise in the list of
+if 2016 - age == year_born
+  puts "Probably not a vampire."
+else
+  puts "Probably a vampire."
+end
 
 puts "Our company serves garlic. Should we order some for you?"
-garlic = gets.chomp
+order_garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurence?"
-insurence = gets.chomp
+health_insurence = gets.chomp
+
+#first exercise in the list of
+if 2016 - age == year_born
+  puts "Probably not a vampire."
+else
+  puts "Defnitely a vampire."
+end
+
+
