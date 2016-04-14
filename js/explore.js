37dad09c3772c953e35d3716cdc775bbbe0aee5a
Walter Chabla <walterchabla43@gmail.com>
2016-04-14 18:08:24 -0400

    java file createa

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Declaring a Fuction:
+// reverse word, that has a parameter that takes a string.
+// inside the function:
+// we are going to take the string, SPLIT it, then the last letter of the string is going to be SWITCH or REVERSE to first letter and same for the other letters, finally JOIN the letters together at the end.
+// output:
+// to be able to return "olleh"
