0b1e1d815a571684a3bd55fcbd704230ab758720
Walter Chabla <walterchabla43@gmail.com>
2016-04-17 09:29:55 -0400

    reverse function created

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,11 @@
 // we are going to take the string, SPLIT it, then the last letter of the string is going to be SWITCH or REVERSE to first letter and same for the other letters, finally JOIN the letters together at the end.
 // output:
 // to be able to return "olleh"
+
+function reverse_word(word) {
+  return word.split('').reverse().join('');
+}
+
+console.log(reverse_word("walter"));
+
+// I have some help from google to write the function.
