f833a859b4d97479e87a4f19f1c260a52a42c678
Walter Chabla <walterchabla43@gmail.com>
2016-04-22 08:54:09 -0400

    hash created, stores the answers

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,7 +5,8 @@
 # name = gets.chomp (use this gets.chompfor all questions, use different variables)
 # if answer is equal to a symbol(key) ask for another answer and update the hash
 # save their answer and put into a hash:
-# declare a new hash with symbols and answers as values: applicant = {name: name, age: age, likes_animals: animals, decor theme: theme, room: room}
+# declare a new hash with symbols and answers as values:
+# applicant = {name: name, age: age, likes_animals: animals, decor theme: theme, room: room}
 # prints the hash with all the information
 
 puts "What is your name?"
@@ -16,14 +17,23 @@ if name == "name"
 end
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "Do you like animals, true or false?"
 animals = gets.chomp
+if animals == "true"
+  animals = true
+elsif animals == "false"
+  animals = false
+end
 
 puts "What is your decor theme?"
 theme = gets.chomp
 
 puts "Which room would you like to decorate?"
 room = gets.chomp
+
+applicant = {name: name, age: age, likes_animals: animals, decor_theme: theme, room: room}
+
+puts applicant
 
