77b669c666156c8c087ecf3103005f8d67e751b8
Walter Chabla <walterchabla43@gmail.com>
2016-04-22 00:39:32 -0400

    swapcase method suspect found

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,50 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-swap_case = "iNvEsTiGaTiOn".swapcase
-puts swap_case
-# => "InVeStIgAtIoN"
+swapcase = "iNvEsTiGaTiOn".swapcase
+puts swapcase
+puts "=> “InVeStIgAtIoN”"
 
-inser = "zom".insert(2, 'o')
-puts inser
-# => "zoom"
+# "zom".<???>
+# => “zoom”
 
-cent = "enhance".center(15)
-puts cent
-#=> "    enhance    "
+# "enhance".<???>
+# => "    enhance    "
 
-up_case = "Stop! You’re under arrest!".upcase
-puts up_case
+# "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-str = "the usual"
-str.replace "the usual suspects"
-puts str
+# "the usual".<???>
 #=> "the usual suspects"
 
-sus = " suspects"
-sus.prepend("the usual")
-puts sus
-#=> "the usual suspects"
+# " suspects".<???>
+# => "the usual suspects"
 
-chop = "The case of the disappearing last letter".chop
-puts chop
+# "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
 
-delete = "The mystery of the missing first letter".delete("T")
-puts delete
+# "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-squeeze = "Elementary  ,    my   dear      Watson!".squeeze(" ")
-puts squeeze
+# "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-letter = "z".ord
-puts letter
+# "z".<???>
 # => 122
-# Is the number on the alphabet and starts with 98 and fineshes on 122.
 # (What is the significance of the number 122 in relation to the character z?)
 
-string = "How many times does the letter 'a' appear in this string?".count("a")
-puts string
-#=> 4
+# "How many times does the letter 'a' appear in this string?".<???>
+# => 4
