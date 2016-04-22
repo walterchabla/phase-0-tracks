a2cce0ff0c880aaffb46747fd5b55d03f7396768
Walter Chabla <walterchabla43@gmail.com>
2016-04-22 12:23:42 -0400

    index 2 deleted from array

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,4 +1,11 @@
+# 1)Initializing new array
 cars = []
 p cars
+
+# 2) Addiding items to the array
 cars << "Mustang" << "Lambo" << "Audi" << "Jeep" << "Aston Martin"
+p cars
+
+# 3) Deleting item at index 2
+cars.delete_at(2)
 p cars
