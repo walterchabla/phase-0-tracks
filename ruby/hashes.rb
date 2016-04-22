32895e290843cdc01b644922186be4fcb095a45f
Walter Chabla <walterchabla43@gmail.com>
2016-04-22 08:41:10 -0400

    pseudocode created for release 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+# A program that will allow an interior designer to enter the details of a given client: the client's name, age, has children, decor theme, which room?
+# ask 5 questions for the designer or user about name, age, has children,decor theme and which room?
+# store the answer:
+# name = gets.chomp (use this gets.chompfor all questions, use different variables)
+# if answer is equal to a symbol(key) ask for another answer and update the hash
+# save their answer and put into a hash:
+# declare a new hash with symbols and answers as values: applicant = {name: name, age: age, children: children, decor theme: theme, room: room}
+# prints the hash with all the information
+
+
