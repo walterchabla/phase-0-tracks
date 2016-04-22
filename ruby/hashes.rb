3f9da653d4ab2e7ed9da993a6d708cb5cfbfce47
Walter Chabla <walterchabla43@gmail.com>
2016-04-22 08:46:49 -0400

    question created for the user and storing their answers

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,29 @@
-# A program that will allow an interior designer to enter the details of a given client: the client's name, age, has children, decor theme, which room?
-# ask 5 questions for the designer or user about name, age, has children,decor theme and which room?
+# Release 3: Use a Hash in a Program
+
+# ask 5 questions for the designer or user about name, age, likes animals,decor theme and which room?
 # store the answer:
 # name = gets.chomp (use this gets.chompfor all questions, use different variables)
 # if answer is equal to a symbol(key) ask for another answer and update the hash
 # save their answer and put into a hash:
-# declare a new hash with symbols and answers as values: applicant = {name: name, age: age, children: children, decor theme: theme, room: room}
+# declare a new hash with symbols and answers as values: applicant = {name: name, age: age, likes_animals: animals, decor theme: theme, room: room}
 # prints the hash with all the information
 
+puts "What is your name?"
+name = gets.chomp
+if name == "name"
+  puts "Please put a valid name?"
+  name = gets.chomp
+end
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "Do you like animals, true or false?"
+animals = gets.chomp
+
+puts "What is your decor theme?"
+theme = gets.chomp
+
+puts "Which room would you like to decorate?"
+room = gets.chomp
 
