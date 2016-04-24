31123fda582b8b37769ffff9c5efe308de158169
Walter Chabla <walterchabla43@gmail.com>
2016-04-24 11:26:18 -0400

    pseudocode done

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,37 @@
+# Release 0: Attempt a Tricky Algorithm
+# a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it
+
+# a method that swaps names, method name:
+# swap_name that takes one parameter(name)
+# inside the method:
+# name.split.rotate.join(" ")
+# name is going to be split and return an array, then the array its going to be rotated last join then and that is going to give a string.
+# output:
+# name swaped
+
+# a method that changes a vowel to the net vowel, method name:
+# next_vowel that takes one parameter (vowels)
+# inside the method:
+# vowels.tr('aeiou', 'eioua')
+# it will swith vowels to the next vowel
+# output:
+# vowels changed to the next vowel
+
+# method that changes a consonant to next consonant, method name:
+# next_consonant that takes a parameter (consonant)
+# inside the method:
+# consonant.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
+# it will swith to the next consonant
+# output:
+# consonants changed to the next consonants
+
+# a method that creates a fake name for the agent, method name:
+# fake_agent_name, that takes a parameter (name)
+# inside the method:
+# using the other methods that wer created
+# fake_name = next_consonant(next_vowel(swap_name(name)))
+# puts fake_name
+# output:
+# creates a fake agent name with vowels and consonants change to the next letter and name swaped.
+
+
