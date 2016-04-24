ac9f6b0690b4683c3783aa63529855059fd9d401
Walter Chabla <walterchabla43@gmail.com>
2016-04-24 12:05:40 -0400

    fake agent name method created

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,6 +6,7 @@
 # inside the method:
 # name.split.rotate.join(" ")
 # name is going to be split and return an array, then the array its going to be rotated last join then and that is going to give a string.
+# end the method
 # output:
 # name swaped
 def swap_name(name)
@@ -17,6 +18,7 @@ end
 # inside the method:
 # vowels.tr('aeiou', 'eioua')
 # it will swith vowels to the next vowel
+# end the method
 # output:
 # vowels changed to the next vowel
 def next_vowel(vowels)
@@ -28,6 +30,7 @@ end
 # inside the method:
 # consonants.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
 # it will swith to the next consonant
+# end the method
 # output:
 # consonants changed to the next consonants
 def next_consonant(consonants)
@@ -37,16 +40,27 @@ end
 # a method that creates a fake name for the agent, method name:
 # fake_agent_name, that takes a parameter (name)
 # inside the method:
-# using the other methods that wer created
-# deal with upcases
+# using the other methods that were created to create a fake name
+# deal with upcases:
 # word = name.downcase
 # fake_name = next_consonant(next_vowel(swap_name(word)))
-# puts fake_name.capitalize
+# puts fake_name.split.map(&:capitalize).join(' ')
+
+# end the method
 # output:
 # creates a fake agent name with vowels and consonants change to the next letter and name swaped.
+def fake_agent_name(name)
+  word = name.downcase
+  fake_name = next_consonant(next_vowel(swap_name(word)))
+  puts fake_name.split.map(&:capitalize).join(' ')
+
+end
+
 
 #driver code
 
 p swap_name("walter chabla")
 p next_vowel("walter chabla")
 p next_consonant("walter chabla")
+fake_agent_name("walter chabla")
+fake_agent_name("Felicia Torres")
