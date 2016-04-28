7d5fd6dce7d3422001e190371caf1b692a7d7a44
Walter Chabla <walterchabla43@gmail.com>
2016-04-27 22:29:20 -0400

    pseudocode a Puppy class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,1 +1,15 @@
-
+# Release 0: Design a Class
+# Name the class:
+# class Puppy
+# The class is going to have some methods and attributes
+# Attributes:
+# Name, age
+# Methods:
+# def initialize method
+# @name = name
+# @age = age
+# end
+# def speak method
+# puts "Something to say"
+# end
+# end the class.
