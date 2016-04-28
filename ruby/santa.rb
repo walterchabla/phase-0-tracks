0fe077bddfdb9e31fe34d32cac27411601946649
Walter Chabla <walterchabla43@gmail.com>
2016-04-27 22:54:33 -0400

    pseudocode a santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+# creating a class
+# class Santa
+# has three methods
+# method name:
+# def speak no parameters
+# inside the method:
+# puts "Ho, ho, ho! Haaaappy holidays!"
+# end
+# method name:
+# def eat_milk_and_cookies has a parameter (cookie)
+# inside the method
+# puts "That was a good #{cookie}"
+# end
+# method name:
+# initialize no parameter
+# inside the method
+# puts "Initializing Santa instance..."
+
