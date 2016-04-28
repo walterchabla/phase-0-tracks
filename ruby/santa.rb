376facd21dfbe65e1aed90cf46372da8488c0550
Walter Chabla <walterchabla43@gmail.com>
2016-04-27 22:57:56 -0400

    class Santa created with some methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,3 +16,22 @@
 # inside the method
 # puts "Initializing Santa instance..."
 
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}"
+  end
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+end
+
+# driver code
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("chocolate chip")
