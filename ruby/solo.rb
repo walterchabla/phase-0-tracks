0e31c5c51a39532d3af6c3edc605a69431714fd6
Walter Chabla <walterchabla43@gmail.com>
2016-04-28 22:55:19 -0400

    first_fruit method created

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,7 @@
 # 6.4, Release 0: Design a Class
 # class of Smoothie
 # three attributes using two data types:
-# fruit, another fruit, blender
+# first_fruit, second_fruit, blender
 # three methods, one of which takes an argument:
 # blend as method with arguments
 # fruit can be a method
@@ -12,20 +12,27 @@
 # Release 1: Write Your Class
 class Smoothie
 
-  #initialize method created with two parameters
+  #method name: initialize,
+  #created with two parameters
   def initialize(fruit, another_fruit)
     @fruit = fruit
     @another_fruit = another_fruit
     puts "testing"
   end
-end
-fruit = Smoothie.new("apple", "pear")
 
-  # fruit method created that prints a statement
+  # method name: first_fruit, no parameters,
+  # output: prints a statement
+  def first_fruit
+    puts "Grabing the first fruit and the fruit is: #{@fruit}."
+  end
 
 
-  # another_fruit method created that prints a statement
+  # second_fruit method created that prints a statement
 
 
   # blender method that blends the fruit and another_fruit and creates the smoothie.
+end
 
+# driver code
+fruit = Smoothie.new("apple", "pear")
+fruit.first_fruit
