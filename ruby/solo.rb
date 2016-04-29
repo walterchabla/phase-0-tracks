e5ffcfae6714a60d8d77675e7bb4cb988bd14fbf
Walter Chabla <walterchabla43@gmail.com>
2016-04-28 21:24:37 -0400

    pseudocode a classs with some methods

---
+++ ruby/solo.rb
@@ -0,0 +1,24 @@
+# 6.4, Release 0: Design a Class
+# class of Smoothie
+# three attributes using two data types:
+# fruit, another fruit, blender
+# three methods, one of which takes an argument:
+# blend as method with arguments
+# fruit can be a method
+# another fruit can be a method
+# make a smoothie with two fruits and a blender.
+#use initialize
+
+# Release 1: Write Your Class
+
+
+  #initialize method created with two parameters
+
+  # fruit method created that prints a statement
+
+
+  # another_fruit method created that prints a statement
+
+
+  # blender method that blends the fruit and another_fruit and creates the smoothie.
+
