9e01ccf8b3e23165c13da4adf1ea6914a532bb3e
Walter Chabla <walterchabla43@gmail.com>
2016-04-28 23:44:38 -0400

    prompt the user for fruits, storing smoothies on a array, printing smoothies that were made when exit

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -20,7 +20,7 @@ class Smoothie
   def initialize(fruit, another_fruit)
     @fruit = fruit
     @another_fruit = another_fruit
-    puts "testing"
+    #puts "testing"
   end
 
   # method name: first_fruit, no parameters,
@@ -51,7 +51,50 @@ class Smoothie
 end
 
 # driver code
-smothie = Smoothie.new("apple", "pear")
-#smothie.first_fruit
-#smothie.second_fruit
-smothie.blender
+#mix = Smoothie.new("apple", "pear")
+#mix.first_fruit
+#mix.second_fruit
+#mix.blender
+
+# Release 2: Use Your Class in a Program
+# prompt the user to use the program
+# array created to store smoothies made
+smoothies_stored = []
+idx = 0
+# looping until user does not want more smoothies
+while idx != "yes"
+puts "Welcome to the smoothie bar!"
+puts "You can mix two fruits"
+puts "What is your first fruit choice?"
+choice_one = gets.chomp
+
+puts "What is your second fruit choice?"
+choice_two = gets.chomp
+# instance of a class created and being store in smoothies_stored array
+smoothie = Smoothie.new(choice_one, choice_two)
+smoothie.blender
+smoothies_stored << smoothie
+# p smoothies_stored
+
+puts "If you would like another smoothie type 'yes'. Otherwise type 'done' if you do not want more smoothies."
+answer = gets.chomp
+if answer == "yes"
+  idx += 1
+else
+  # printing each smoothie that was made
+  smoothies_stored.each {|smoothie|
+    puts "This #{smoothie} smoothie was made"}
+  exit
+end
+end
+
+
+
+
+
+
+
+
+
+
+
