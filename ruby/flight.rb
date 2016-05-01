cabb3fc621e7d3aafdc8a218afd9dd8ff74acd16
Walter Chabla <walterchabla43@gmail.com>
2016-05-01 13:35:33 -0400

    Flight module created

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,3 +1,11 @@
+# 'Flight' odule created
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reacing #{altitude} ..."
+  end
+end
+
+# two classes created
 class Bird
 end
 
