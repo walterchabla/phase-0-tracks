0ea1920e89c90b603c98f40340bad6c3a462ea10
Walter Chabla <walterchabla43@gmail.com>
2016-05-01 13:29:55 -0400

    Yelling happy method created

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,17 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now
+
+  # a yelling angry method
+  def self.yell_angrily(words)
+    words + "!!! + :("
+  end
+
+  # a yelling happy method
+  def self.yelling_happily(word)
+    word + "!!! + :D"
+  end
+
 end
+
+p Shout.yell_angrily("HOW")
+p Shout.yelling_happily("HIII")
