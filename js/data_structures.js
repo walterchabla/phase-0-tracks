881db24ad5b14ef004a4ff0b3b28b9bc9b41a780
Walter Chabla <walterchabla43@gmail.com>
2016-05-09 04:02:38 -0400

    adding items to the arrays and printing the arrays with items added

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,13 @@
 var colors = ["brown", "white", "pink", "lavender"];
 
 var horse_names = ["Aeris", "Oberon", "Jag", "Umbra"];
+
+// Test Code
+console.log(colors);
+console.log(horse_names);
+
+colors.push("Blue with stripes");
+horse_names.push("Lad");
+
+console.log(colors);
+console.log(horse_names);
