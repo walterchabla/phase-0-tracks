ff462831e7c5eec06e6f00640f1a32540560ccbc
Walter Chabla <walterchabla43@gmail.com>
2016-05-09 06:03:30 -0400

    pseudocode created for function longest_word

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+//Release 0: Find the Longest Word
+// Write a function that takes an array of words or phrases and returns the longest word in the array.
+// Declare a name for the function:
+// LONGEST_WORD(PARAMETER)
+// Input for the function:
+// Array that contains strings, I'm going to use a FOR loop, .LENGTH to see how long is the array. Im going to use .LENGTH on each string to see how long is the string in the array, and then using an IF statement comparison to print the largest string in the array .
+// Output:
+// Prints the longest string.
