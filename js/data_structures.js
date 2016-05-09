438bbe98130367be6705493241c7b844d1a5af6f
Walter Chabla <walterchabla43@gmail.com>
2016-05-09 04:52:39 -0400

    created an object horses, stores horses names and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+//Release 0: Work with Arrays
+
 var colors = ["brown", "white", "pink", "lavender"];
 
 var horse_names = ["Aeris", "Oberon", "Jag", "Umbra"];
@@ -11,3 +13,11 @@ horse_names.push("Lad");
 
 console.log(colors);
 console.log(horse_names);
+
+//Release 1: Build an Object
+var horses = {};
+
+for (var i = 0; horse_names[i] && colors[i]; i++){
+  horses[horse_names[i]] = colors[i];
+};
+console.log(horses)
