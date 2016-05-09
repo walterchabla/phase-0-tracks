c3fca295c7f19305dc11574a90ef78d9cc06268f
Walter Chabla <walterchabla43@gmail.com>
2016-05-09 05:19:53 -0400

    constructur function created, classlike behavior

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,9 +15,28 @@ console.log(colors);
 console.log(horse_names);
 
 //Release 1: Build an Object
+console.log("---------------")
 var horses = {};
 
 for (var i = 0; horse_names[i] && colors[i]; i++){
   horses[horse_names[i]] = colors[i];
 };
 console.log(horses)
+
+console.log("---------------")
+function Car(color, model, doors) {
+  console.log("New car:", this);
+  this.color = color;
+  this.model = model;
+  this.doors = doors;
+  this.start = function() {
+    console.log("BROOM!!");
+  };
+
+  console.log("CAR CREATED");
+}
+
+var anotherCar = new Car("White", "Auidi", "2 Doors");
+console.log(anotherCar);
+console.log("Car model is: " + anotherCar.model + ".", "Car color is: " + anotherCar.color + ".", "Car has number of doors: " + anotherCar.doors + ".")
+anotherCar.start();
