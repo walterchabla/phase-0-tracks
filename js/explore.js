ee1728c0724cc1135dc0d75f8ce212b6833dc6ac
Walter Chabla <walterchabla43@gmail.com>
2016-05-09 03:43:39 -0400

    js file created, with js syntax of function and if statement

--- js/explore.js
+++ js/explore.js
@@ -1,18 +1,23 @@
 // Declaring a Fuction:
-// reverse word, that has a parameter that takes a string.
+// reverse, that has a parameter that takes a string.
 // inside the function:
 // we are going to take the string, SPLIT it, then the last letter of the string is going to be SWITCH or REVERSE to first letter and same for the other letters, finally JOIN the letters together at the end.
 // output:
-// to be able to return "olleh"
+// to be able to return "olleh" if "hello was entered"
 
-function reverse_word(word) {
-  return word.split('').reverse().join('');
+function reverse(string) {
+  var str = string.split('').reverse('').join('');
+  console.log(str);
 }
 
-var new_word = reverse_word("Walter");
-console.log(new_word);
-// I have some help from google to write the function.
 
-if (2 && 2) {
-  console.log(new_word);
+// Test code
+var reversed = reverse("hello");
+
+if (5 == 5) {
+  console.log("5 is equal to 5");
+} else {
+  console.log("5 is not equal to 5");
 }
+
+
