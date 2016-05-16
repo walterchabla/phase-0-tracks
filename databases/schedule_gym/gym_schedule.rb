cca88230bfe9f18de9060414b8826ebac616ff14
Walter Chabla <walterchabla43@gmail.com>
2016-05-16 06:34:29 -0400

    user interface for users to type their information

--- databases/schedule_gym/gym_schedule.rb
+++ databases/schedule_gym/gym_schedule.rb
@@ -29,7 +29,7 @@ end
 # testing method
 # insert_schedule(db, "Monday", "2 hours", "It hurts now but one day it will be your warm up. -MGQ")
 
-# method retrieve_data that retrieves data from schedule table
+# method retrieve_data that retrieves data from schedule table and prints in user friendly way
 def retrieve_data(db)
   schedules = db.execute("SELECT * FROM schedule")
   schedules.each do |schedule|
@@ -39,4 +39,18 @@ def retrieve_data(db)
 end
 
 # testing method
-retrieve_data(db)
+# retrieve_data(db)
+
+#User interface
+
+puts "Nice job on going to the gym!\nLets keep track of days and hours at the gym so you can see your progress.\nIf you need to update information about day or hour type 'update' otherwise press enter to continue"
+answer = gets.chomp
+
+puts "Day that you went to the gym?"
+day = gets.chomp
+
+puts "How many hours did you spent in the gym?"
+hours = gets.chomp
+
+puts "Write an inspirational quote to inspire you to go to the gym tomorrow."
+quote = gets.chomp
