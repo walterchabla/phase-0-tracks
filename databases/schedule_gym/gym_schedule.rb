14bc3ced70a5d5b0fcdff3e41e1ad831d67e7d7d
Walter Chabla <walterchabla43@gmail.com>
2016-05-16 06:16:55 -0400

    retrieve_data method created to print data from schedules table

--- databases/schedule_gym/gym_schedule.rb
+++ databases/schedule_gym/gym_schedule.rb
@@ -21,10 +21,22 @@ SQL
 # create schedule table in database
 db.execute(create_table_cmd)
 
-# method insert_schedule with 4 arguments that adds schedule to the table
+# method insert_schedule with 4 arguments that adds data to schedule table
 def insert_schedule(db, went_day, hours_spend, inspirational_quote)
   db.execute("INSERT INTO schedule (went_day, hours_spend, inspirational_quote) VALUES (?, ?, ?)", [went_day, hours_spend, inspirational_quote])
 end
 
 # testing method
-insert_schedule(db, "Monday", "2 hours", "It hurts now but one day it will be your warm up. -MGQ")
+# insert_schedule(db, "Monday", "2 hours", "It hurts now but one day it will be your warm up. -MGQ")
+
+# method retrieve_data that retrieves data from schedule table
+def retrieve_data(db)
+  schedules = db.execute("SELECT * FROM schedule")
+  schedules.each do |schedule|
+    puts "--------------------------------"
+    puts "Day went to the gym: #{schedule['went_day']}\nHours spent in the gym: #{schedule['hours_spend']}\nInspirational quote of the day: #{schedule['inspirational_quote']}"
+    end
+end
+
+# testing method
+retrieve_data(db)
