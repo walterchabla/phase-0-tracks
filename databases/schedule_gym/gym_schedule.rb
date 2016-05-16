d6ac8d1fd63c72393a2e279b2a44c5cc7b118f70
Walter Chabla <walterchabla43@gmail.com>
2016-05-16 05:47:58 -0400

    method insert_schedule to insert data into database

--- databases/schedule_gym/gym_schedule.rb
+++ databases/schedule_gym/gym_schedule.rb
@@ -13,10 +13,15 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS schedule(
     id INTEGER PRIMARY KEY,
     went_day VARCHAR(15),
-    hour_spend VARCHAR(30),
-    inspiration_quoate VARCHAR(255)
+    hours_spend VARCHAR(30),
+    inspirational_quote VARCHAR(255)
     )
 SQL
 
 # create schedule table in database
 db.execute(create_table_cmd)
+
+# method insert_schedule with 4 arguments that adds schedule to the table
+def insert_schedule(db, went_day, hours_spend, inspirational_quote)
+  db.execute("INSERT INTO schedule (went_day, hours_spend, inspirational_quote) VALUES (?, ?, ?)", [went_day, hours_spend, inspirational_quote])
+end
