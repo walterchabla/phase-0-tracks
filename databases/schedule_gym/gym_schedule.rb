cf7a981f4f180c7c8701d42b153e8fa694136732
Walter Chabla <walterchabla43@gmail.com>
2016-05-16 05:29:15 -0400

    table created using ruby in schedule.db file

--- databases/schedule_gym/gym_schedule.rb
+++ databases/schedule_gym/gym_schedule.rb
@@ -1,4 +1,4 @@
-#Releases 0-3: Build Something Awesome
+# Releases 0-3: Build Something Awesome
 
 # require gems
 require 'sqlite3'
@@ -7,3 +7,16 @@ require 'faker'
 # created SQLite3 database
 db = SQLite3::Database.new("schedule.db")
 db.results_as_hash = true
+
+# fancy string delimiters to create a table for schedule.db
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS schedule(
+    id INTEGER PRIMARY KEY,
+    went_day VARCHAR(15),
+    hour_spend VARCHAR(30),
+    inspiration_quoate VARCHAR(255)
+    )
+SQL
+
+# create schedule table in database
+db.execute(create_table_cmd)
