e868fe08dd0bfdedeea4194b880e556ccf07b5a4
Walter Chabla <walterchabla43@gmail.com>
2016-05-20 15:14:25 -0400

    added a table, and a post method

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -10,6 +10,7 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  @campus = db.execute("SELECT * FROM campus")
   erb :home
 end
 
@@ -17,11 +18,22 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/add/campus' do
+  erb :campus
+end
+
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/campus' do
+  db.execute("INSERT INTO campus (location) VALUES (?)", params['location'])
+  redirect '/'
+end
+
 
 # add static resources
